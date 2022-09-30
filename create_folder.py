9152d277e3aefe72a8f89715ee21cc15bb6b7955
Alvaro Jose HAHN MENACHO <alvaro.hahn@list.lu>
2022-09-30 16:42:16 +0200

    First commit folders creator

---
+++ create_folder.py
@@ -0,0 +1,59 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Sep 30 14:03:51 2022
+
+@author: hahn
+"""
+
+
+import shutil
+from pathlib import Path
+
+
+def create_folder():
+    
+    proj_path_path = Path.cwd() / 'reproducible_lca'
+    proj_path_path.mkdir(parents=True, exist_ok=False)
+    
+    dev_path = Path.cwd() / 'reproducible_lca' / 'dev'
+    dev_path.mkdir(parents=True, exist_ok=False)
+    
+    config_path = Path.cwd() / 'reproducible_lca' / 'config'
+    config_path.mkdir(parents=True, exist_ok=False)
+    
+    envs_path = Path.cwd() / 'reproducible_lca' / 'envs'
+    envs_path.mkdir(parents=True, exist_ok=False)
+    
+    resources_path = Path.cwd() / 'reproducible_lca' / 'resources'
+    resources_path.mkdir(parents=True, exist_ok=False)
+    
+    scripts_path = Path.cwd() / 'reproducible_lca' / 'scripts'
+    scripts_path.mkdir(parents=True, exist_ok=False)
+    
+def create_zip():
+    
+    output_filename = 'reproducible_lca'
+    dir_name = 'reproducible_lca'
+    shutil.make_archive(output_filename, 'zip', dir_name)
+    
+def create_snakefile():
+    
+    snkf = open("reproducible_lca\Snakefile.txt", "w")
+    snkf.write("rule run:\n\tinput:\n\t\t'workflow/config/config.json'\n\n\toutput:\n\t\t'workflow/results/results.txt'\n\tconda:\n\t\t'envs/env.yaml'\n\tthreads: 1\n\tscript:\n\t\t'scripts/script.py'")
+    snkf.close()
+
+def create_script():
+    script = open("reproducible_lca\scripts\script.txt", "w")
+    script.write('import bw2io, bw2data, bw2calc\nimport eidl\nfrom pathlib import Path\nimport json\n\nCONFIG_PATH = Path(__file__).parent.parent / "config"\nRESOURCES_PATH = Path(__file__).parent.parent / "resources"\nRESULTS_PATH = Path(__file__).parent.parent / "results"\n\nwith open(CONFIG_PATH / "config.json") as f:\n\tconfig = json.load(f)\n\nprint("Creating project...")\
+                 \n\nbw2data.projects.set_current("default")\n\nprint("Installing biosphere...")\nprint(bw2data.databases)\nbw2io.bw2setup()\n\n\ndef install_dbs(db):\n\n\tif db["available in resources"]:\n\t\tif all(i in bw2data.databases for i in db["depends on"]):\n\t\t\tprint(f"Importing {db[\'name\']}...")\n\t\t\tbw2io.BW2Package.import_file \
+                 (RESOURCES_PATH / db["file name"])\n\t\t\ else:\n\t\t\traise ValueError(f"Missing databases in order to import {db[\'name\']}.")\n\telse:\n\t\tif db["name"] == "ecoinvent" and all(i in bw2data.databases for i in db["depends on"]):\n\t\t\tname = db["name"]\n\t\t\tver = db["version"]\n\t\t\tsystem_model = db["system model"]\n\t\t\tif f"{name} {system_model} {ver}" not in \
+                 bw2data.databases:\n\t\t\t\teidl.get_ecoinvent(db_name=f"{name} {system_model} {ver}",auto_write=True,version=ver,system_model=system_model,)\n\t\t\t\telse\n\t\t\t\t\tprint(f"{name} {ver} {system_model} already present.")\n\t\t\telse:\n\t\t\t\traise \
+                 ValueError("Not implemented yet")\n\n\ndef run_lca(lca_setup):\n\tdb = lca_setup["demand"]["database"]\n\tcode = lca_setup["demand"]["code"]\n\tqty = lca_setup["demand"]["quantity"]\n\tact = bw2data.get_activity((db, code))\n\tmethod = bw2data.methods.random()\n\tlca = bw2calc.LCA({act: qty}, method)\n\tlca.lci()\n\tlca.lcia()\n\treturn lca.score\n\ndbs = \
+                 config[0]["databases"]\n\nprint("Running LCA...")\nwith open(RESULTS_PATH / "results.txt", "w") as f:\n\tfor lca_setup in config:\n\t\tf.write(str(run_lca(lca_setup)))')
+
+    script.close()
+    
+create_folder()
+create_snakefile()
+create_script()
+#create_zip()
